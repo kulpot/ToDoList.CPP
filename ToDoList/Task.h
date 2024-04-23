fa60d5fa8214cf14c156346367ec7189d0849311
Kulpot <kulpotwahu@gmail.com>
2024-04-24 01:54:04 +0800

    Before Creating Project ->
    //Requirements: Open Folder->C:\Users\sunny\wxWidgets-\build\msw ->click:wx_vc16.sln
    //build: Debug->32->Release->32/Debug->x64->Release->x64
    //File->New->Project  //ref link:Ep1 Installing wxWidgets -->https://www.youtube.com/watch?v=ONYW3hBbk-8&list=PLFk1_lkqT8MbVOcwEppCPfjGOGhLvcf9G&index=3
    //View->Other Windows->Property Manager->right click:ToDoList->Add Existing Property Sheet..->C:\Users\wxWidgets-\wxwidgets.props

--- ToDoList/Task.h
+++ ToDoList/Task.h
@@ -3,6 +3,8 @@
 //Requirements: Open Folder->C:\Users\sunny\wxWidgets-\build\msw ->click:wx_vc16.sln
 //build: Debug->32->Release->32/Debug->x64->Release->x64
 //File->New->Project		//ref link:Ep1 Installing wxWidgets -->https://www.youtube.com/watch?v=ONYW3hBbk-8&list=PLFk1_lkqT8MbVOcwEppCPfjGOGhLvcf9G&index=3 
+//View->Other Windows->Property Manager->right click:ToDoList->Add Existing Property Sheet..->C:\Users\wxWidgets-\wxwidgets.props
+
 
 class Task
 {
