49faa00dba720dbc5dc53d73ece6b8eb97953931
Kulpot <kulpotwahu@gmail.com>
2024-04-25 18:37:07 +0800

    Task.h->ctrl+. ->
    void saveTasksToFile(const std::vector<Task>& tasks, const std::string& filename);
    ->ctrl+. ->
    
    std::vector<Task> loadTasksFromFile(const std::string& fileName);
    
    Task.cpp->
    
    #include "Task.h"
    
    void saveTasksToFile(const std::vector<Task>& tasks, const std::string& filename)
    {
    }
    
    std::vector<Task> loadTasksFromFile(const std::string& fileName)
    {
     return std::vector<Task>();
    }

--- ToDoList/Task.cpp
+++ ToDoList/Task.cpp
@@ -1,1 +1,10 @@
 #include "Task.h"
+
+void saveTasksToFile(const std::vector<Task>& tasks, const std::string& filename)
+{
+}
+
+std::vector<Task> loadTasksFromFile(const std::string& fileName)
+{
+	return std::vector<Task>();
+}
