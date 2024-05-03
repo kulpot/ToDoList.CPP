3c4d24daa88ac9db1860a5224554639e050dc66f
Kulpot <kulpotwahu@gmail.com>
2024-05-04 03:54:18 +0800

    MainFrame.h->
     void SwapTasks(int i, int j);

--- ToDoList/MainFrame.h
+++ ToDoList/MainFrame.h
@@ -16,6 +16,7 @@ private:
 	void AddTaskFromInput();
 	void DeleteSelectedTask();
 	void MoveSelectedTask(int offset);
+	void SwapTasks(int i, int j);
 
 	wxPanel* panel;
 	wxStaticText* headlineText;
